33ea3ff9ecfda469d32eb8291d985d45d95c6ce9
thunder <34943968+nethackcom@users.noreply.github.com>
2023-09-10 16:42:52 +0300

    Update BinarySearch.py

--- Алгоритмы/Базовые алгоритмы/BinarySearch.py
+++ Алгоритмы/Базовые алгоритмы/BinarySearch.py
@@ -1,1 +1,15 @@
+def BinarySeach(arr: list, value: int) -> int:
+  first = 0
+  last = arr[len(value)]-1
+  index = -1
 
+  while (first <= last) and (index == -1):
+    mid = (first+last)//2
+    if arr[mid] == value:
+      index = mid
+    else:
+      if arr[mid] < value:
+        first = mid + 1
+      elif arr[mid] > value:
+        last = mid - 1
+    return index
